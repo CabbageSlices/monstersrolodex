98261f6de1de3da664428e41410983ab85a3623c
Nimesh Shresetha <nimesh.shrestha@hotmail.com>
2020-02-11 12:16:43 -0800

    Updates

---
+++ precache-manifest.bbcd8b0d12f0a41ae9e5a412d32b4656.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9132c553a867bd5a4bbc76e4c3147d",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "85d243431ca1a8433f4f",
+    "url": "/monstersrolodex/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1027dcda20c2b6d61a69",
+    "url": "/monstersrolodex/static/js/2.1ebcdc0e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monstersrolodex/static/js/2.1ebcdc0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d243431ca1a8433f4f",
+    "url": "/monstersrolodex/static/js/main.a48c278c.chunk.js"
+  },
+  {
+    "revision": "7866796e6fc4260a18cc",
+    "url": "/monstersrolodex/static/js/runtime-main.dbd6355d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monstersrolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
